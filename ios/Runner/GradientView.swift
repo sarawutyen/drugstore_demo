a73f6cdbc208c7bd55cdde8322932ecaa2f4692b
sarawut_yen <sarawutyencpe@gmail.com>
2024-07-06 14:03:42 +0700

    Add LSApplicationQueriesSchemes

---
+++ ios/Runner/GradientView.swift
@@ -0,0 +1,8 @@
+//
+//  GradientView.swift
+//  Runner
+//
+//  Created by Ahmad Yen on 6/7/2567 BE.
+//
+
+import Foundation
---
+++ ios/Runner/LaunchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchViewController.swift
+//  Runner
+//
+//  Created by Ahmad Yen on 6/7/2567 BE.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
